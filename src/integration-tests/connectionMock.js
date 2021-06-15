a5c8a57cb33c364f9c3937a6d31d399aaca501ab
Daniel <danieldsrd@gmail.com>
2021-06-15 17:29:02 -0300

    Inicio dos testes

---
+++ src/integration-tests/connectionMock.js
@@ -0,0 +1,16 @@
+const { MongoClient } = require('mongodb');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
+let connectionMock;
+const DBServer = new MongoMemoryServer();
+
+const getConnection = async () => {
+  const URLMock = await DBServer.getUri();
+  const OPTIONS = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+  return connectionMock ? connectionMock : MongoClient.connect(URLMock, OPTIONS);
+};
+
+module.exports = { getConnection };
